47a0e0628c4be7a2971ab627d1ac1ff28dcd8d05
An <an.arletta.cattie@gmail.com>
2018-12-05 12:25:05 +0100

    no message

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let n = 4;
+let x = "Web Developer";
+let y = "Spanje";
+let z = "Isaura";
 
+console.log("U wordt "+ x +" en woont in " + y +", en bent getrouwd met " + z +" met "+ n+ " kinderen.");
+
+let h = 2018;
+let g = 1993;
+
+console.log(h - g)
+
+let l = 25;
+let m = 80;
+
+console.log("Je hebt nog steeds " + (m-l) + " jaar voorat je de leeftijd van " + m + " jaar bereikt hebt.")
+
+let resultaat = (1 + 2) * 3 + 4 / 2;
+
+console.log(1+2)
+console.log(3*3)
+console.log(9+4)
+console.log(13/2)
