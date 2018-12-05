2efbdfd35c557e81c0c05244a65c753e6f6d01cc
An <an.arletta.cattie@gmail.com>
2018-12-05 16:27:35 +0100

    no message

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ let z = "Isaura";
 
 console.log("U wordt "+ x +" en woont in " + y +", en bent getrouwd met " + z +" met "+ n + " kinderen.");
 
+
+
 let h = 2018;
 let g = 1993;
 
 console.log(h - g)
 
+
+
 let l = 25;
 let m = 80;
 
 console.log("Je hebt nog steeds " + (m-l) + " jaar voorat je de leeftijd van " + m + " jaar bereikt hebt.")
+
+
 
 let resultaat = (1 + 2) * 3 + 4 / 2;
 
@@ -21,3 +27,51 @@ console.log(1+2);
 console.log(3*3);
 console.log(4/2);
 console.log(9+2);
+
+
+
+let language = "fr";
+let message;
+
+if (language === "fr"){
+  message="Bonjour tout le monde";
+}
+
+else if (language === "nl"){
+  message="Hallo iedereen";
+}
+
+else {
+  message="Hello World";
+}
+
+console.log(message);
+
+
+let score = 78;
+let result;
+
+if (score >= 90){
+  result="A";
+}
+else if (score < 90){
+  result="B";
+}
+else if (score <=50) {
+  result="C";
+}
+
+console.log(result);
+
+let hond = "chihuahua";
+let aantal = 2;
+let uitkomst;
+
+if (aantal === 1){
+  uitkomst=hond;
+}
+else{
+  uitkomst=hond +"s";
+}
+
+console.log(uitkomst);
